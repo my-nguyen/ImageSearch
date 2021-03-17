b6fcb8a8ff618b1b98e311f0fe9df92e2676492c
My Nguyen <nguyen_my@yahoo.com>
2021-03-16 17:07:21 -0700

    Part 9

--- app/src/main/java/com/florian_walther/imagesearch/GalleryFragment.kt
+++ app/src/main/java/com/florian_walther/imagesearch/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package com.florian_walther.mvvmimagesearch
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.florian_walther.imagesearch.GalleryViewModel
@@ -12,7 +15,7 @@ import com.florian_walther.imagesearch.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     val viewModel by viewModels<GalleryViewModel>()
     var _binding: FragmentGalleryBinding? = null
@@ -22,13 +25,14 @@ class GalleryFragment: Fragment(R.layout.fragment_gallery) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setHasOptionsMenu(true)
         _binding = FragmentGalleryBinding.bind(view)
 
         val adapter = UnsplashAdapter()
         binding.apply {
             recyclerView.setHasFixedSize(true)
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
-                header=LoadAdapter { adapter.retry() }, footer=LoadAdapter { adapter.retry() }
+                header = LoadAdapter { adapter.retry() }, footer = LoadAdapter { adapter.retry() }
             )
         }
 
@@ -40,5 +44,34 @@ class GalleryFragment: Fragment(R.layout.fragment_gallery) {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_gallery, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    binding.recyclerView.scrollToPosition(0)
+
+                    // viewModel.searchPhotos() updates viewModel.currentQuery which triggers
+                    // the switchmap which executes repository.searchPhotos() which updates
+                    // viewModel.photos LiveData which this Fragment observes and so will be notified
+                    // of updates
+                    viewModel.searchPhotos(query)
+
+                    // hide the keyboard
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
     }
 }
